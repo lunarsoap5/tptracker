6570e9abf4b066838ba3c8e24f2691749d5140aa
lunarsoap5 <40438826+lunarsoap5@users.noreply.github.com>
2020-05-26 15:25:55 -0500

    2.0

---
+++ var_maptracker/scripts/init.lua
@@ -0,0 +1,26 @@
+local variant = Tracker.ActiveVariantUID
+local has_map = variant ~= "var_minimalist" and (not variant:find("itemsonly"))
+
+Tracker:AddItems("items/common.json")
+Tracker:AddItems("items/dungeon_items.json")
+
+ScriptHost:LoadScript("scripts/logic.lua")
+
+if has_map then
+    Tracker:AddMaps("maps/maps.json")
+    Tracker:AddLocations("locations/overworld.json")
+	Tracker:AddLocations("locations/poes.json")
+	Tracker:AddLocations("locations/bugs.json")
+	Tracker:AddLocations("locations/shops.json")
+    Tracker:AddLocations("locations/dung_reg.json")
+    Tracker:AddLocations("locations/dung_mq.json")
+    Tracker:AddLayouts("layouts/options.json")
+end
+
+
+Tracker:AddLayouts("layouts/item_grids.json")
+Tracker:AddLayouts("layouts/layouts.json")
+Tracker:AddLayouts("layouts/capture.json")
+
+Tracker:AddLayouts("layouts/tracker.json")
+Tracker:AddLayouts("layouts/broadcast.json")
