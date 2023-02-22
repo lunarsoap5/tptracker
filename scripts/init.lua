ed3b85f327dd5ffbbbb7d5d500a0fabdfd219ee6
lunarsoap5 <40438826+lunarsoap5@users.noreply.github.com>
2023-02-21 21:59:39 -0600

    Fixed capitalization for certain image files.

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,5 +1,11 @@
 Tracker:AddItems("items/common.json")
 Tracker:AddItems("items/dungeon_items.json")
-Tracker:AddLayouts("layouts/dungeon_grids_keysanity.json")
 Tracker:AddLayouts("layouts/tracker.json")
 Tracker:AddLayouts("layouts/broadcast.json")
+if has_map then
+    Tracker:AddMaps("maps/maps.json")
+    Tracker:AddLocations("locations/overworld.json")
+    Tracker:AddLocations("locations/dung_reg.json")
+    Tracker:AddLayouts("layouts/options.json")
+    Tracker:AddLayouts("layouts/dungeon_grids_keysanity.json")
+end
